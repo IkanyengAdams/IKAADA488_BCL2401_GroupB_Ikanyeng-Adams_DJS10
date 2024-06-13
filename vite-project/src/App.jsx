d65dadcdc0bb62cbceea3cacc6a9530cca844276
Ikanyeng Adams <ikanyengadams2@gmail.com>
2024-06-13 21:51:36 +0200

    Added initial implementation of App component with state and effect to fetch posts from API

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from 'react';
+
+
+function App() {
+  const [posts, setPosts] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+ fetch('https://jsonplaceholder.typicode.com/posts')
+ .then(response => {
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  } else {
+    return response.json();
+  }
+})
